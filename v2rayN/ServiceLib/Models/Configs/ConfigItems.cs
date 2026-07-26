02b4ccb7c702781f10275b69c4c8f3b952a652e3
freekof <freekof8@gmail.com>
2026-07-26 22:56:40 +0800

    feat: hide IP info column by default

--- v2rayN/ServiceLib/Models/Configs/ConfigItems.cs
+++ v2rayN/ServiceLib/Models/Configs/ConfigItems.cs
@@ -103,7 +103,7 @@ public class UIItem
     public bool MacOSShowInDock { get; set; }
     public List<ColumnItem> MainColumnItem { get; set; }
     public List<WindowSizeItem> WindowSizeItem { get; set; }
-    public bool HideColumnIpInfo { get; set; }
+    public bool HideColumnIpInfo { get; set; } = true;
 }
 
 [Serializable]
